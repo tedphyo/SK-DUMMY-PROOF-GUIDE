707d3961be06e18e592a3d1d501ee99f633c1aa6
Kyaw <58354193+wajeht@users.noreply.github.com>
2020-08-12 02:06:30 -0500

    Update Program.cs

--- skRaid/Program.cs
+++ skRaid/Program.cs
@@ -13,15 +13,12 @@ namespace Delete_Cusom_File
             string[] reseffectFiles = { "Dmg_GTI_Bomb.nif", "BurnPain.nif", "BurnPainDmg.nif", "StaBurnPainLoop.nif", "StaBurnPainMe_Last.nif", "StaBurnPainMeLoop.nif", "StaMobACS01.nif", "StaMobACS01Loop.nif", "StaMobMRS01.nif", "StaMobMRS01Loop.nif", "StaMobMSD02Loop.nif", "StaNortfBurn.nif", "WeddingMark01.nif", "StaBlueDragon.nif", "StaRedDragon.nif", "RedDragonProtection.nif", "Sta_BlueDragon.nif", "Sta_RedDragon.nif", "WeddingMark01.nif", "Aura100.nif", "Aura120.nif", "ItemEffect007.nif", "ItemEffect027.nif", "ItemEffect047.nif", "ItemEffect065.nif", "ItemEffect077.nif", "ItemEffect087.nif", "ItemEffect097.nif", "StaBurnPainMeLoop.nif", "StaBurnPainMe_Last.nif", "BurnPainDmg.nif", "BurnPainMe.nif", "KazhulEffect01.nif", "KazhulEffect02.nif" };
 
             var itemEffectInsdeReseffectFolder = @"*ItemEffect*.nif";
-
             var reseffectFolder = Environment.CurrentDirectory + @"\reseffect";
             var resitemFolder = Environment.CurrentDirectory + @"\resitem";
             var femaleFolderInsideResitem = Environment.CurrentDirectory + @"\resitem\Female";
             var maleFolderInsideResitem = Environment.CurrentDirectory + @"\resitem\Male";
             var commonFolderInsideReschar = Environment.CurrentDirectory + @"\reschar\Common";
-
             var currentFolder = Environment.CurrentDirectory;
-
 
             try
             {
@@ -35,7 +32,6 @@ namespace Delete_Cusom_File
                         Console.WriteLine(file + " has been deleted!");
                     }
                 }
-
 
                 // delete itemeeffect.nif files inside resefffect
                 foreach (string file in Directory.GetFiles(reseffectFolder, itemEffectInsdeReseffectFolder))
@@ -55,7 +51,6 @@ namespace Delete_Cusom_File
                     Console.WriteLine(commonFolderInsideReschar + " folder has been deleted!");
                 }
 
-
                 // delete files insde resitem folder
                 foreach (string file in Directory.GetFiles(resitemFolder))
                 {
@@ -68,11 +63,8 @@ namespace Delete_Cusom_File
                 Console.WriteLine("You are in " + currentFolder + " Folder!");
                 Console.WriteLine("Place the sk.exe program inside FiestaOnline main folder!");
             }
-
-
-
+            
             Console.ReadKey();
-
         }
     }
 }
